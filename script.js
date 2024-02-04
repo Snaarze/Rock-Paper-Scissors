abeea4a9a2f809e4791d5acdb98d4169096f9d62
Jeremy Glenn I. Panlasigui <panlasiguijeremyglenn49@gmail.com>
2024-02-04 20:28:51 +0800

    Added new Features, player now can chooce choice on buttons, also the game will end after whoever reach 5 points first

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const buttons = document.querySelectorAll('button');
+let playerScore = 0;
+let computerScore = 0 ;
+
+
+buttons.forEach((button) => {
+  
+button.addEventListener('click', (e) =>{
+        let playerChoice =  e.target.innerText;
+       console.log(playRound(playerChoice));
+     })
+   
+})
+
+function ResetScore(){
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('player-score').innerHTML = `${playerScore}`;
+    document.getElementById('computer-score').innerHTML = `${computerScore}`;
+}
+
 
 function getComputerChoice(){
     let randomNumber = Math.random() * 3
@@ -14,52 +35,66 @@ function getComputerChoice(){
 }
 
 
-function playRound(){
+function playRound(playerChoice){
 
 
-    let userInput = prompt("What's your Move? :", "");
-    let playerSelection = userInput.toLowerCase();
+    let playerSelection = playerChoice.toLowerCase();
     let computerChoice = getComputerChoice();
     let result = "";
+
+
+    if(playerScore >= 5 || computerScore >= 5){
+        alert("Game is Over")
+        return;
+    } 
 
     if(playerSelection === 'rock'){
         if(computerChoice  === 'scissor'){
             result = "You win! Rock beat Scissor";
+            playerScore++;
         }else if(computerChoice === 'paper'){
             result = "You lose ! Paper beat rock";
+            computerScore++
         }else if(computerChoice === 'rock'){
             result = "Tie";
         }
     }else if(playerSelection === 'paper'){
         if(computerChoice === 'rock'){
             result = "You win! Paper beat Rock";
+            playerScore++;
         }else if(computerChoice === 'scissor'){
             result = "You lose! Scissor beat Paper";
+            computerScore++
         }else if(computerChoice === 'paper') {
             result = "Tie";
         }
     }else if(playerSelection === 'scissor'){
         if(computerChoice === 'paper'){
             result = "You win!  Scissor beat Paper";
+            playerScore++;
         }else if(computerChoice === 'rock'){
             result = "You lose! Rock beat Scissor";
+            computerScore++
         }else if(computerChoice === 'scissor'){
             result = "Tie";
         }
-    }else {
-        alert("Invalid Output : Please choose rock/paper/scissor ")
-    }
+    };
+
     if(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissor"){
         document.getElementById("playerChoice").innerHTML = `${playerSelection}`;
         document.getElementById("computerChoice").innerHTML = `${computerChoice}`;
         document.getElementById("result").innerHTML = `${result}`;
+        document.getElementById('player-score').innerHTML = `${playerScore}`;
+        document.getElementById('computer-score').innerHTML = `${computerScore}`;
     }else {
         document.getElementById("playerChoice").innerHTML = "";
         document.getElementById("computerChoice").innerHTML = "";
         document.getElementById("result").innerHTML = "";
-    }
-   
+    };
 }
+
+
+
 
 
 
